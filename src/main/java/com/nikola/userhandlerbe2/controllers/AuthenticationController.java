4a205f0dd97378d0c848f4fedf122b75d0c68992
NikiKerezov <nikolaaleksandrov123@gmail.com>
2023-12-29 22:03:44 +0200

    Added security and jwt filter

---
+++ src/main/java/com/nikola/userhandlerbe2/controllers/AuthenticationController.java
@@ -0,0 +1,11 @@
+package com.nikola.userhandlerbe2.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userHandler/v2/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+}
--- src/main/java/com/nikola/userhandlerbe2/repositories/UserRepository.java
+++ src/main/java/com/nikola/userhandlerbe2/repositories/UserRepository.java
@@ -4,10 +4,13 @@ import com.nikola.userhandlerbe2.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends MongoRepository<User, Long> {
+import java.util.Optional;
 
-    User findByEmail(String email);
-    User findByFirstName(String firstName);
-    User findByLastName(String lastName);
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByFirstName(String firstName);
+    Optional<User> findByLastName(String lastName);
 }
